c8bf9b44a1151cb3c8545144581d90e915c869b0
danielpinoy <almirante.danieljohn@gmail.com>
2025-06-05 12:25:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// const http = require("http");
+const http = require("http");
 const app = require("./app");
-// const server = http.createServer(app);
-const serverless = require("serverless-http");
-// const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-// server.listen(port, () => console.log("server is running on port 3000"));
+const port = process.env.PORT || 3000;
 
-module.exports.handler = serverless(app);
+server.listen(port, () => console.log("server is running on port 3000"));
